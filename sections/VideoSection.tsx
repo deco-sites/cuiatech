01b19c29031a8f6f7189da003d450a6539078b0b
Jeshua Ferreira <dev.09@pbhub.com.br>
2024-05-21 20:30:05 +0000

    Release staging environment with 0 changes made by Jeshua Ferreira

--- sections/VideoSection.tsx
+++ sections/VideoSection.tsx
@@ -10,12 +10,14 @@ export default function ConnectingBridges({
   subtitle,
   description,
   urlSource,
+  width = "500",
+  height = "300"
 }: Props) {
   return (
     <section class="w-full py-[20px] px-[20px] items-center md:px-[30px] md:py-[60px] flex flex-col md:flex-row md:justify-center md:gap-[40px] gap-[20px] bg-gray-100">
       <div class="flex flex-col md:items-start md:w-[50%] w-full gap-[15px] md:gap-[30px]">
         <div class="2xl:w-[70%]">
-          <h2 class="text-3xl md:text-[42px] text-primary-550 font-sans text-center md:text-start">
+          <h2 class="text-3xl md:text-[42px] text-primary-550 font-sans text-center md:text-start leading-[48px]">
             SOMOS{" "}
             <strong class="text-3xl md:text-[42px] text-primary-550 font-sans ">
               PONTE
@@ -27,7 +29,7 @@ export default function ConnectingBridges({
           </h2>
         </div>
 
-        <div class="flex flex-col md:flex-col gap-[10px] w-full 2xl:w-[70%]">
+        <div class="flex flex-col md:flex-col gap-[10px] w-full xl:w-[65%] 2xl:w-[60%]">
           {subtitle && <p class="md:text-[16px] text-primary-550">{subtitle}
           </p>}
           {description && (
@@ -36,7 +38,7 @@ export default function ConnectingBridges({
         </div>
       </div>
 
-      <video width="500" height="300" controls>
+      <video width={width} height={height} controls>
         <source src={urlSource} type="" />
         Your browser does not support the video tag.
       </video>
