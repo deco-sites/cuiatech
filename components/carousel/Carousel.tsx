bade4d375b7c1558885cef27f4a3028fd91db51d
Elysson Marconi <ellyssonmiike@hotmail.com>
2024-05-27 19:22:11 -0300

    feat: added colaborators depositions section.

--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -1,55 +1,27 @@
-import { ImageWidget } from "apps/admin/widgets.ts";
-import { signal } from "@preact/signals";
-
-export type Slide = Array<{
-  title: string;
-  image: ImageWidget;
-}>;
+import Deposition, {
+  Props as DepositionsProps,
+} from "site/components/ui/Deposition.tsx";
 
 export interface Props {
-  slides: Slide[];
+  depositions: DepositionsProps[];
 }
 
-export default function Component({ slides }: Props) {
+export default function Component({ depositions }: Props) {
   return (
     <>
-      <div className="carousel w-full lg:container flex justify-center lg:max-w-7xl lg:mx-auto mx-5 md:mx-10">
-        {slides.map((images, index) => (
-          <div
-            id={"slide" + index}
-            className="carousel-item relative w-full flex justify-center"
-          >
-            <div class="flex justify-center items-center w-full gap-4">
-              {images.map((image, index) => (
-                <div class="w-[50%] h-60 flex rounded-[33px] bg-primary-500">
-                </div>
-              ))}
+      <div class="relative w-full bg-base-200">
+        <div className="cuia-scrollbar flex justify-start p-8 gap-12 overflow-x-scroll">
+          {depositions.map(({ description, image }, index) => (
+            <div class="">
+              <Deposition
+                description={description}
+                image={image}
+                color={index % 2 === 0 ? "primary" : "secondary"}
+                key={`fst-${index}`}
+              />
             </div>
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a
-                href={"#slide" +
-                  (index - 1 < 0 ? slides.length - 1 : index - 1)}
-                className="btn btn-circle"
-              >
-                ❮
-              </a>
-              <a
-                href={"#slide" + (index + 1 >= slides.length ? 0 : index + 1)}
-                className="btn btn-circle"
-              >
-                ❯
-              </a>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="flex justify-center w-full py-2 gap-2">
-        {slides.map((_, index) => (
-          <a
-            href={"#slide" + index}
-            className="w-2 h-2 bg-secondary-500 rounded-full cursor-pointer"
-          />
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
